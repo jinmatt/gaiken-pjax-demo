ff6046a6c79e2c18d64429eca35eb18f892be765
Jinsu Mathew <jinsu@askme.in>
2015-06-05 11:59:14 +0530

    next page added

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var gaikan = require('gaikan');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/next', function(req, res, next) {
+  res.render('next', { title: 'Next' });
 });
 
 router.get('/content', function(req, res, next) {
