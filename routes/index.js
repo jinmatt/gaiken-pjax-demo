9b0c89fe911394586bb9353f5db2d5a1daca7612
Jinsu Mathew <jinsu@askme.in>
2015-06-05 12:54:32 +0530

    Pjax now loads gaiken template for next page from home page.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var gaikan = require('gaikan');
+var debug = require('debug')('gaikan-pjax:server');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  gaikan.options.layout = './views/layout.html';
   res.render('index', { title: 'Home' });
 });
 
 router.get('/next', function(req, res, next) {
-  res.render('next', { title: 'Next' });
+  if (req.header('X-PJAX')) {
+    gaikan.options.layout = null;
+    return res.render('next', { title: 'Next' });
+  }
+
+  gaikan.options.layout = './views/layout.html';
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/content', function(req, res, next) {
